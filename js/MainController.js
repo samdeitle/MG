a0ad2a883832b7acfd96a83154fc60e181b75bfa
samdeitle <sgd@uoregon.edu>
2018-05-30 19:28:41 -0700

    initial push to github. page with pics on it

---
+++ js/MainController.js
@@ -0,0 +1,78 @@
+app.controller('MainController',
+['$scope',function($scope) {
+  $scope.title	 = 'Dirty! Old! Balls!';
+  $scope.pictures= [
+    								{
+                      link: 'img/anime/1.png',
+                    },
+    								{
+                      link: 'img/anime/2.jpg',
+                    },
+                    {
+                      link: 'img/anime/3.png',
+                    },
+                    {
+                      link: 'img/anime/4.png',
+                    },
+                    {
+                      link: 'img/anime/5.jpg',
+                    },
+                    {
+                      link: 'img/anime/6.jpg',
+                    },
+                    {
+                      link: 'img/anime/7.jpg',
+                    },
+                    {
+                      link: 'img/anime/8.jpg',
+                    },
+                    {
+                      link: 'img/anime/9.jpg',
+                    },
+                    {
+                      link: 'img/anime/10.jpg',
+                    },
+                    {
+                      link: 'img/anime/11.jpg',
+                    },
+                    {
+                      link: 'img/anime/12.jpg',
+                    },
+                    {
+                      link: 'img/anime/13.png',
+                    },
+                    {
+                      link: 'img/anime/14.png',
+                    },
+                    {
+                      link: 'img/anime/15.png',
+                    },
+                    {
+                      link: 'img/anime/16.png',
+                    },
+                    {
+                      link: 'img/anime/17.png',
+                    },
+                    {
+                      link: 'img/anime/18.png',
+                    },
+                    {
+                      link: 'img/anime/19.png',
+                    },
+                    {
+                      link: 'img/anime/20.png',
+                    },
+                    {
+                      link: 'img/anime/21.png',
+                    },
+                    {
+                      link: 'img/anime/22.jpg',
+                    },
+  								];
+  $scope.plusOne = function(index) {
+    $scope.products[index].likes += 1;
+  };
+  $scope.minusOne = function(index) {
+    $scope.products[index].dislikes += 1;
+  };
+}]);
